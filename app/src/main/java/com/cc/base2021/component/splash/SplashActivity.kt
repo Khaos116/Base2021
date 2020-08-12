b7ab8da97104b39c99430860c32244c8d2066c0f
case <442415631@qq.com>
2020-08-12 14:31:34 +0700

    增加启动页

---
+++ app/src/main/java/com/cc/base2021/component/splash/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.cc.base2021.component.splash
+
+import com.cc.base2021.R
+import com.cc.base2021.comm.CommActivity
+
+/**
+ * Author:case
+ * Date:2020/8/12
+ * Time:13:56
+ */
+class SplashActivity : CommActivity() {
+
+  //<editor-fold defaultstate="collapsed" desc="XML">
+  override fun layoutResId() = R.layout.activity_splash
+  //</editor-fold>
+
+  //<editor-fold defaultstate="collapsed" desc="初始化View">
+  override fun initView() {
+  }
+  //</editor-fold>
+
+  //<editor-fold defaultstate="collapsed" desc="初始化数据">
+  override fun initData() {
+  }
+  //</editor-fold>
+}
