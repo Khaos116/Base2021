03e1c229fc1f5379074f85d566895c7e2feb01b1
case <442415631@qq.com>
2020-08-19 19:41:01 +0700

    增加注释

--- base/src/main/java/com/cc/base/ext/OtherExt.kt
+++ base/src/main/java/com/cc/base/ext/OtherExt.kt
@@ -30,6 +30,7 @@ fun FragmentManager.getContext(): Context? {
 //停止惯性滚动
 fun RecyclerView.stopInertiaRolling() {
   try {
+    //如果是Support的RecyclerView则需要使用"cancelTouch"
     val field = this.javaClass.getDeclaredMethod("cancelScroll")
     field.isAccessible = true
     field.invoke(this)
