d9fabd0b0cc98699cdd4b425b8062d1a09156462
case <442415631@qq.com>
2020-08-28 13:31:39 +0700

    更新版本

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -7,15 +7,15 @@ object Versions {
 }
 
 object Deps {
-  //根目录gradle
+  //根目录gradle https://maven.aliyun.com/mvn/search
   const val plugin_android_gradle = "com.android.tools.build:gradle:4.0.1"
   const val plugin_kotlin_gradle = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
-  const val plugin_r8_gradle = "com.android.tools:r8:2.0.99" //更新R8版本，解决正式版无法打包的问题 https://github.com/square/okhttp/issues/4604
+  const val plugin_r8_gradle = "com.android.tools:r8:2.1.66" //更新R8版本，解决正式版无法打包的问题 https://github.com/square/okhttp/issues/4604
 
   //使用kotlin
   const val kotlin_stdlib = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlin}"
 
-  //系统相关
+  //系统相关 https://maven.aliyun.com/mvn/search
   const val core_ktx = "androidx.core:core-ktx:1.3.1"
   const val activity_ktx = "androidx.activity:activity-ktx:1.2.0-alpha07"
   const val appcompat = "androidx.appcompat:appcompat:1.3.0-alpha01"
@@ -43,7 +43,7 @@ object Deps {
   const val immersionbar_ktx = "com.gyf.immersionbar:immersionbar-ktx:3.0.0"
 
   //数据存储 https://github.com/Tencent/MMKV
-  const val mmkv = "com.tencent:mmkv-static:1.2.1"
+  const val mmkv = "com.tencent:mmkv-static:1.2.2"
 
   //数据解析 https://github.com/google/gson
   const val gson = "com.google.code.gson:gson:2.8.6"
@@ -59,25 +59,25 @@ object Deps {
   //RxAndroid https://github.com/ReactiveX/RxAndroid
   const val rxandroid = "io.reactivex.rxjava2:rxandroid:2.1.1"
 
-  //动态权限请求 https://github.com/guolindev/PermissionX
-  const val permissionx = "com.permissionx.guolindev:permissionx:1.3.0"
+  //动态权限请求 https://github.com/getActivity/XXPermissions
+  const val permissions = "com.hjq:xxpermissions:8.2"
 
   //EventBus https://github.com/JeremyLiao/LiveEventBus
   const val eventBus = "com.jeremyliao:live-event-bus-x:1.7.2"
 
   //WebView https://github.com/Justson/AgentWeb
-  const val agentweb = "com.just.agentweb:agentweb:4.1.3"
+  const val agentweb = "com.just.agentweb:agentweb:4.1.4"
 
   //侧滑 https://github.com/luckybilly/SmartSwipe
   const val swipe = "com.billy.android:smart-swipe:1.1.2"
   const val swipex = "com.billy.android:smart-swipe-x:1.1.0"
 
   //图片加载 https://github.com/coil-kt/coil
-  const val coil = "io.coil-kt:coil:0.11.0"
+  const val coil = "io.coil-kt:coil:0.12.0"
 
   //多类型适配器 https://github.com/drakeet/MultiType
   const val multitype = "com.drakeet.multitype:multitype:4.2.0"
 
   //SVGA动画 https://github.com/svga/SVGAPlayer-Android/blob/master/readme.zh.md
-  const val svga = "com.github.yyued:SVGAPlayer-Android:2.5.8"
+  const val svga = "com.github.yyued:SVGAPlayer-Android:2.5.9"
 }
