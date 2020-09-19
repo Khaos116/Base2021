eab6861806b944edcaf516e278d4eee617c79e0b
CASE <442415631>
2020-09-19 13:40:31 +0700

    增加视频资源

---
+++ app/src/main/java/com/cc/base2021/bean/local/VideoBean.kt
@@ -0,0 +1,13 @@
+package com.cc.base2021.bean.local
+
+/**
+ * Description:
+ * @author: caiyoufei
+ * @date: 2019/12/12 11:45
+ */
+class VideoBean(
+  var id: Long = 0,
+  var title: String? = null,
+  var url: String? = null,
+  var thumb: String? = null
+)
