477b8d57d2310499ead9d31c8170da6f84abecac
CASE <442415631>
2020-09-22 18:35:40 +0700

    吐司文字提取到XML

--- app/src/main/java/com/cc/base2021/component/splash/SplashActivity.kt
+++ app/src/main/java/com/cc/base2021/component/splash/SplashActivity.kt
@@ -2,8 +2,8 @@ package com.cc.base2021.component.splash
 
 import android.content.Context
 import android.media.AudioManager
+import com.blankj.utilcode.util.StringUtils
 import com.blankj.utilcode.util.TimeUtils
-import com.cc.utils.PermissionUtils
 import com.cc.base2021.R
 import com.cc.base2021.comm.CommActivity
 import com.cc.base2021.component.guide.GuideActivity
@@ -11,6 +11,7 @@ import com.cc.base2021.component.main.MainActivity
 import com.cc.base2021.utils.MMkvUtils
 import com.cc.base2021.utils.RxUtils
 import com.cc.ext.*
+import com.cc.utils.PermissionUtils
 import com.gyf.immersionbar.ktx.immersionBar
 import com.hjq.permissions.*
 import com.opensource.svgaplayer.SVGACallback
@@ -29,7 +30,7 @@ class SplashActivity : CommActivity() {
   //<editor-fold defaultstate="collapsed" desc="变量">
   //一个小时变一张图
   private val randomImg = TimeUtils.millis2String(System.currentTimeMillis())
-      .split(" ")[1].split(":")[0].toInt()
+    .split(" ")[1].split(":")[0].toInt()
 
   //倒计时
   private var countTime = 3L
@@ -57,37 +58,40 @@ class SplashActivity : CommActivity() {
 
   //<editor-fold defaultstate="collapsed" desc="初始化View">
   override fun initView() {
-    (getSystemService(Context.AUDIO_SERVICE) as AudioManager).adjustStreamVolume(AudioManager.STREAM_MUSIC,
-        AudioManager.ADJUST_MUTE, 0) //静音
+    (getSystemService(Context.AUDIO_SERVICE) as AudioManager).adjustStreamVolume(
+      AudioManager.STREAM_MUSIC,
+      AudioManager.ADJUST_MUTE, 0
+    ) //静音
     //UI显示出来再执行倒计时和权限判断
     mContentView.post {
       hasSDPermission = PermissionUtils.instance.hasSDPermission()
       //请求SD卡权限
       if (!hasSDPermission) {
         XXPermissions.with(this)
-            .permission(Permission.MANAGE_EXTERNAL_STORAGE)
-            .request(object : OnPermission {
-              override fun hasPermission(granted: MutableList<String>, all: Boolean) {
-                if (PermissionUtils.instance.hasSDPermission()) {
-                  hasSDPermission = true
-                  goNextPage()
-                } else {
-                  "必须要给予SD卡权限才能使用".toast()
-                  finish()
-                }
-              }
-
-              override fun noPermission(denied: MutableList<String>, quick: Boolean) {
-                if (quick) {
-                  "被永久拒绝授权，请手动授予存储权限".toast()
-                  // 如果是被永久拒绝就跳转到应用权限系统设置页面
-                  XXPermissions.startPermissionActivity(mActivity, denied);
-                } else {
-                  "必须要给予SD卡权限才能使用".toast()
-                }
+          .permission(Permission.MANAGE_EXTERNAL_STORAGE)
+          .request(object : OnPermission {
+            override fun hasPermission(granted: MutableList<String>, all: Boolean) {
+              if (PermissionUtils.instance.hasSDPermission()) {
+                hasSDPermission = true
+                goNextPage()
+              } else {
+                StringUtils.getString(R.string.permission_sdcard).toast()
                 finish()
               }
-            })
+            }
+
+            override fun noPermission(denied: MutableList<String>, quick: Boolean) {
+              if (quick) {
+                R.string.action_loading
+                StringUtils.getString(R.string.permission_sdcard_never).toast()
+                // 如果是被永久拒绝就跳转到应用权限系统设置页面
+                XXPermissions.startPermissionActivity(mActivity, denied);
+              } else {
+                StringUtils.getString(R.string.permission_sdcard_must).toast()
+              }
+              finish()
+            }
+          })
       }
     }
     splashTv.click {
@@ -115,8 +119,8 @@ class SplashActivity : CommActivity() {
       override fun onStep(frame: Int, percentage: Double) {}
     }
     disposable = Observable.timer(2, TimeUnit.SECONDS)
-        .compose(RxUtils.instance.rx2SchedulerHelperO())
-        .subscribe { splashTv.visible() }
+      .compose(RxUtils.instance.rx2SchedulerHelperO())
+      .subscribe { splashTv.visible() }
   }
   //</editor-fold>
 
@@ -143,8 +147,10 @@ class SplashActivity : CommActivity() {
     super.finish()
     disposable?.dispose()
     splashSVGA?.callback = null
-    (getSystemService(Context.AUDIO_SERVICE) as AudioManager).adjustStreamVolume(AudioManager.STREAM_MUSIC,
-        AudioManager.ADJUST_UNMUTE, 0) //非静音
+    (getSystemService(Context.AUDIO_SERVICE) as AudioManager).adjustStreamVolume(
+      AudioManager.STREAM_MUSIC,
+      AudioManager.ADJUST_UNMUTE, 0
+    ) //非静音
   }
   //</editor-fold>
 }
