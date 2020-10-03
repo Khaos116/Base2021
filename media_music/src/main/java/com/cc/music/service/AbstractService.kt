973103d66e657e01f4251d670e8e234bec52f55e
CASE <442415631@qq.com>
2020-10-03 14:25:08 +0700

    增加打印lib及初始化

--- media_music/src/main/java/com/cc/music/service/AbstractService.kt
+++ media_music/src/main/java/com/cc/music/service/AbstractService.kt
@@ -3,6 +3,8 @@ package com.cc.music.service
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import com.cc.BuildConfig
+import timber.log.Timber
 
 /**
  * https://blog.csdn.net/CrazyMo_/article/details/80250565
@@ -13,7 +15,10 @@ import android.os.IBinder
 abstract class AbstractService : Service() {
   protected var mBinder: IBinder? = null
   override fun onBind(intent: Intent?): IBinder? {
-    if (mBinder == null) mBinder = initBinder()
+    if (mBinder == null) {
+      mBinder = initBinder()
+      if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
+    }
     return mBinder //与客户端成功连接上的时候返回给客户端使用的对象
   }
 
