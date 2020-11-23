400322ba6fdb6ef837273b86d1b46b210b8b4dcb
CASE <442415631@qq.com>
2020-11-23 13:07:21 +0700

    升级版本

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -2,8 +2,8 @@ object Versions {
   internal const val sdkMin = 23
   internal const val sdkTarget = 30
   internal const val kotlin = "1.4.10"
-  internal const val okHttp = "4.8.1"
-  internal const val rxHttp = "2.4.1"
+  internal const val okHttp = "4.9.0"
+  internal const val rxHttp = "2.4.4"
   internal const val coil = "1.0.0"
 }
 
@@ -11,35 +11,35 @@ object Deps {
   //根目录gradle https://maven.aliyun.com/mvn/search
   const val plugin_android_gradle = "com.android.tools.build:gradle:4.1.1"
   const val plugin_kotlin_gradle = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
-  const val plugin_r8_gradle = "com.android.tools:r8:2.1.66" //更新R8版本，解决正式版无法打包的问题 https://github.com/square/okhttp/issues/4604
+  const val plugin_r8_gradle = "com.android.tools:r8:2.1.67" //更新R8版本，解决正式版无法打包的问题 https://github.com/square/okhttp/issues/4604
   const val plugin_pandora = "com.github.whataa:pandora-plugin:1.0.0"
 
   //使用kotlin
   const val kotlin_stdlib = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlin}"
 
   //系统相关 https://maven.aliyun.com/mvn/search
-  const val core_ktx = "androidx.core:core-ktx:1.5.0-alpha03"
-  const val activity_ktx = "androidx.activity:activity-ktx:1.2.0-alpha08"
   const val appcompat = "androidx.appcompat:appcompat:1.3.0-alpha02"
-  const val fragment = "androidx.fragment:fragment:1.3.0-alpha08"
-  const val material = "com.google.android.material:material:1.3.0-alpha02"
-  const val constraint = "androidx.constraintlayout:constraintlayout:2.0.1"
+  const val core_ktx = "androidx.core:core-ktx:1.5.0-alpha05"
+  const val activity_ktx = "androidx.activity:activity-ktx:1.2.0-beta01"
+  const val fragment = "androidx.fragment:fragment:1.3.0-beta01"
+  const val material = "com.google.android.material:material:1.3.0-alpha03"
+  const val constraint = "androidx.constraintlayout:constraintlayout:2.1.0-alpha1"
 
   //启动初始化 https://developer.android.google.cn/topic/libraries/app-startup
-  const val startup = "androidx.startup:startup-runtime:1.0.0-beta01"
+  const val startup = "androidx.startup:startup-runtime:1.0.0-rc01"
 
   //分包 https://developer.android.google.cn/studio/build/multidex?hl=zh_cn#mdex-gradle
   const val multidex = "androidx.multidex:multidex:2.0.1"
 
   //内存泄漏检测 https://square.github.io/leakcanary/getting_started/
-  const val leakcanary = "com.squareup.leakcanary:leakcanary-android:2.4"
+  const val leakcanary = "com.squareup.leakcanary:leakcanary-android:2.5"
 
   //潘多拉调试工具 https://github.com/whataa/pandora/blob/master/README_CN.md
   const val pandora = "com.github.whataa:pandora:androidx_v2.1.0"
   const val pandora_no = "com.github.whataa:pandora-no-op:v2.0.3"
 
   //工具类 https://github.com/Blankj/AndroidUtilCode/blob/master/lib/utilcode/README-CN.md
-  const val utilcodex = "com.blankj:utilcodex:1.29.0"
+  const val utilcodex = "com.blankj:utilcodex:1.30.5"
 
   //防止混淆 https://github.com/Blankj/FreeProGuard
   const val proguardx = "com.blankj:free-proguard:1.0.2"
@@ -55,7 +55,7 @@ object Deps {
   const val immersionbar_ktx = "com.gyf.immersionbar:immersionbar-ktx:3.0.0"
 
   //数据存储 https://github.com/Tencent/MMKV
-  const val mmkv = "com.tencent:mmkv-static:1.2.2"
+  const val mmkv = "com.tencent:mmkv-static:1.2.5"
 
   //数据解析 https://github.com/google/gson
   const val gson = "com.google.code.gson:gson:2.8.6"
@@ -64,7 +64,8 @@ object Deps {
   const val okhttp = "com.squareup.okhttp3:okhttp:${Versions.okHttp}"
 
   //协程 https://github.com/Kotlin/kotlinx.coroutines
-  const val coroutine = "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9"
+  const val coroutine = "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.1"
+  const val coroutine_android = "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.4.1"
 
   //RxHttp https://github.com/liujingxing/okhttp-RxHttp
   const val rxhttp = "com.ljx.rxhttp:rxhttp:${Versions.rxHttp}"
@@ -75,7 +76,7 @@ object Deps {
   const val rxandroid = "io.reactivex.rxjava2:rxandroid:2.1.1"
 
   //动态权限请求 https://github.com/getActivity/XXPermissions
-  const val permissions = "com.hjq:xxpermissions:8.6"
+  const val permissions = "com.hjq:xxpermissions:9.2"
 
   //EventBus https://github.com/JeremyLiao/LiveEventBus
   const val eventBus = "com.jeremyliao:live-event-bus-x:1.7.2"
@@ -96,16 +97,16 @@ object Deps {
   const val multitype = "com.drakeet.multitype:multitype:4.2.0"
 
   //SVGA动画 https://github.com/svga/SVGAPlayer-Android/blob/master/readme.zh.md
-  const val svga = "com.github.yyued:SVGAPlayer-Android:2.5.12"
+  const val svga = "com.github.yyued:SVGAPlayer-Android:2.5.13"
 
   //图片选择器 https://github.com/LuckSiege/PictureSelector
-  const val pic_select = "com.github.LuckSiege.PictureSelector:picture_library:v2.5.9"
+  const val pic_select = "com.github.LuckSiege.PictureSelector:picture_library:v2.6.0"
 
   //打包资源压缩 https://github.com/smallSohoSolo/McImage/blob/master/README-CN.md
   const val mc_image = "com.smallsoho.mobcase:McImage:1.5.1"
 
   //阿里播放器官网 https://help.aliyun.com/document_detail/124711.html?spm=a2c4g.11186623.6.1085.11b511f9ytuUNH
   //阿里播放器Github https://github.com/alibaba/CicadaPlayer/tree/release/0.1/platform/Android
-  const val ali_player = "com.aliyun.sdk.android:AliyunPlayer:5.1.6-full"
-  const val ali_conan = "com.alivc.conan:AlivcConan:1.0.4"
+  const val ali_player = "com.aliyun.sdk.android:AliyunPlayer:5.2.2-full"
+  const val ali_conan = "com.alivc.conan:AlivcConan:1.0.5"
 }
