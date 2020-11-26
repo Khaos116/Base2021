ebdda6fce1529ebde1ce56e3c3085bb4dd661dde
CASE <442415631@qq.com>
2020-11-26 15:19:18 +0700

    修改为默认数据解析

--- app/src/main/java/com/cc/base2021/rxhttp/parser/ResponseGankParser.kt
+++ app/src/main/java/com/cc/base2021/rxhttp/parser/ResponseGankParser.kt
@@ -1,11 +1,14 @@
 package com.cc.base2021.rxhttp.parser
 
 import com.blankj.utilcode.util.GsonUtils
+import com.cc.base2021.R
 import com.cc.base2021.bean.base.GankResponse
+import okhttp3.Response
 import rxhttp.wrapper.annotation.Parser
 import rxhttp.wrapper.entity.ParameterizedTypeImpl
 import rxhttp.wrapper.exception.ParseException
 import rxhttp.wrapper.parse.AbstractParser
+import rxhttp.wrapper.utils.convert
 import java.io.IOException
 import java.lang.reflect.Type
 import java.nio.charset.StandardCharsets
@@ -41,22 +44,24 @@ open class ResponseGankParser<T> : AbstractParser<T> {
   override fun onParse(response: okhttp3.Response): T {
     //获取泛型类型
     val type: Type = ParameterizedTypeImpl[GankResponse::class.java, mType]
+    //数据转换
+    val gankResponse = response.convert<GankResponse<T>>(type)
     //读取返回结果
-    var result: String? = null
-    val body = response.body
-    if (body != null) {
-      val source = body.source()
-      source.request(Long.MAX_VALUE)
-      result = source.buffer.clone().readString(StandardCharsets.UTF_8)
-    }
-    //判断结果
-    if (result == null || result.isEmpty()) throw ParseException("500", "服务器没有数据", response)
-    //转换类型
-    val data: GankResponse<T> = GsonUtils.fromJson(result, type)
+    //var result: String? = null
+    //val body = response.body
+    //if (body != null) {
+    //  val source = body.source()
+    //  source.request(Long.MAX_VALUE)
+    //  result = source.buffer.clone().readString(StandardCharsets.UTF_8)
+    //}
+    ////判断结果
+    //if (result == null || result.isEmpty()) throw ParseException("500", "服务器没有数据", response)
+    ////转换类型
+    //val data: GankResponse<T> = GsonUtils.fromJson(result, type)
     //获取data字段
-    val list = data.data
+    val data = gankResponse.data
     //code不等于0，说明数据不正确，抛出异常
-    if (data.error() || list == null) throw ParseException("-1", if (data.error()) "接口error" else "数据返回错误", response)
-    return list
+    if (gankResponse.error() || data == null) throw ParseException("-1", if (gankResponse.error()) "接口error" else "数据返回错误", response)
+    return data
   }
 }
--- app/src/main/java/com/cc/base2021/rxhttp/parser/ResponseWanParser.kt
+++ app/src/main/java/com/cc/base2021/rxhttp/parser/ResponseWanParser.kt
@@ -7,6 +7,7 @@ import rxhttp.wrapper.annotation.Parser
 import rxhttp.wrapper.entity.ParameterizedTypeImpl
 import rxhttp.wrapper.exception.ParseException
 import rxhttp.wrapper.parse.AbstractParser
+import rxhttp.wrapper.utils.convert
 import java.io.IOException
 import java.lang.reflect.Type
 import java.nio.charset.StandardCharsets
@@ -42,22 +43,23 @@ open class ResponseWanParser<T> : AbstractParser<T> {
   override fun onParse(response: okhttp3.Response): T {
     //获取泛型类型
     val type: Type = ParameterizedTypeImpl[BaseResponse::class.java, mType]
+    val baseResponse = response.convert<BaseResponse<T>>(type)
     //读取返回结果
-    var result: String? = null
-    val body = response.body
-    if (body != null) {
-      val source = body.source()
-      source.request(Long.MAX_VALUE)
-      result = source.buffer.clone().readString(StandardCharsets.UTF_8)
-    }
-    //判断结果
-    if (result == null || result.isEmpty()) throw ParseException("500", "服务器没有数据", response)
-    //转换类型
-    val data: BaseResponse<T> = GsonUtils.fromJson(result, type)
+    //var result: String? = null
+    //val body = response.body
+    //if (body != null) {
+    //  val source = body.source()
+    //  source.request(Long.MAX_VALUE)
+    //  result = source.buffer.clone().readString(StandardCharsets.UTF_8)
+    //}
+    ////判断结果
+    //if (result == null || result.isEmpty()) throw ParseException("500", "服务器没有数据", response)
+    ////转换类型
+    //val data: BaseResponse<T> = GsonUtils.fromJson(result, type)
     //获取data字段
-    val list = data.data
+    val data = baseResponse.data
     //code不等于0，说明数据不正确，抛出异常
-    if (data.errorCode != 0 || list == null) throw ParseException(data.errorCode.toString(), data.errorMsg, response)
-    return list
+    if (baseResponse.errorCode != 0 || data == null) throw ParseException(baseResponse.errorCode.toString(), baseResponse.errorMsg, response)
+    return data
   }
 }
