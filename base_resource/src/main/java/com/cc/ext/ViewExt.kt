0cf566b442752cb185c98039c7b5be21ebb8173f
CASE <442415631@qq.com>
2021-01-01 16:35:18 +0700

    找到View所在的fragment

--- base_resource/src/main/java/com/cc/ext/ViewExt.kt
+++ base_resource/src/main/java/com/cc/ext/ViewExt.kt
@@ -3,6 +3,9 @@ package com.cc.ext
 import android.graphics.Color
 import android.view.View
 import android.view.ViewManager
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.LifecycleOwner
 import com.cc.R
 import com.cc.utils.PressEffectHelper
 
@@ -106,4 +109,40 @@ fun View.pressEffectDisable() {
 fun View.removeParent() {
   val parentTemp = parent
   if (parentTemp is ViewManager) parentTemp.removeView(this)
+}
+
+//获取所有父类
+fun View?.getMyParents(): MutableList<View> {
+  val parents = mutableListOf<View>()
+  var myParent: View? = this?.parent as? View //找父控件
+  for (i in 0..Int.MAX_VALUE) {
+    if (myParent != null) {
+      parents.add(myParent) //添加到父控件列表
+      myParent = myParent.parent as? View //继续向上查找父控件
+    } else break //找不到View的父控件即结束
+  }
+  return parents
+}
+
+//找到View所在的fragment
+fun View?.getMyFragment(): Fragment? {
+  (this?.context as? FragmentActivity)?.let { ac ->
+    //找到所有上级View
+    val parents = getMyParents()
+    //找到一级(activity嵌套的fragment)fragment
+    val fragments = ac.supportFragmentManager.fragments
+    //再找二级(fragment嵌套的fragment)fragment
+    val list = mutableListOf<Fragment>()
+    list.addAll(fragments)
+    fragments.forEach { c -> list.addAll(c.getAllChildFragments()) }
+    if (list.isNotEmpty()) for (i in list.size - 1 downTo 0) {
+      list[i].view?.let { v -> if (parents.contains(v)) return list[i] }
+    }
+  }
+  return null //如果都找不到，则应该不是放在fragment中，可能直接放在activity中了
+}
+
+//获取生命周期管理
+fun View?.getMyLifecycleOwner(): LifecycleOwner? {
+  return (this?.getMyFragment()) ?: (this?.context as? LifecycleOwner)
 }
